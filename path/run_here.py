727dd73408faf57e9f164aaceb49942abb34a623
Euxcbsks <hawhaw02030746@gmail.com>
2021-03-06 17:08:33 +0800

    Update path/run_here.py, NO DOC Changes:   cancel:     "with … as …" support   add:     dire_offset support

--- path/run_here.py
+++ path/run_here.py
@@ -1,5 +1,5 @@
-import os
-
+from os import getcwd, chdir
+from os.path import split
 from inspect import currentframe
 
 __all__ = [
@@ -7,86 +7,29 @@ __all__ = [
 ]
 
 class run_here():
-	"""**Type**: context manager, class decorator without parameters
-	
-	**Description**: Execute the function in the directory where the function is located
-	
-	**Usage**:
-	
-	- with … as
-	
-		.. code-block:: python
-			:linenos:
-			
-			with run_here(function[, args[, kargs]]) as function_return:
-				pass
-	
-	- decorator
-		.. code-block:: python
-			:linenos:
-			
-			@run_here
-			def func():
-				pass
-	
-	**Example**:
+	def __init__(self, target):
+		target_type = target.__class__.__name__
 		
-		Main file directory: /folder/subfolder/run_here_example.py
-		
-		Another file directory: /folder/target.txt
-	
-	- with run_here(func, *args, **kargs) as …
-		
-		.. code-block:: python
-			:linenos:
-			
-			def relat_open():
-				os.chdir('..')
-				
-				with open('target.txt') as f:
-					return t.read()
-			
-			with run_here(relat_open) as data:
-				print(data)
-	
-	- decorator
-		
-		.. code-block:: python
-			:linenos:
-			
-			@run_here
-			def relat_open():
-				os.chdir('..')
-				
-				with open('target.txt') as f:
-					return t.read()
-			
-			print(relat_open())
-	
-	*run_here will auto restore work directory after function end*
-	"""
-	def __init__(self, func, *args, **kargs):
-		if func.__class__.__name__ != "function":
-			raise TypeError(f"the 'func' must be function, not '{func.__class__.__name__}'")
-		
-		self.func = func
-		self.args = args
-		self.kargs = kargs
-		self.orig_path = os.getcwd()
-		func_frame = currentframe().f_back
-		
-		if func_frame.f_globals['__file__'] == __file__:
-			self.func_path = os.path.split(func_frame.f_back.f_globals['__file__'])[0]
+		if target_type == 'str':
+			self.dire_offset = target
+		elif target_type == 'function':
+			self.func = target
 		else:
-			self.func_path = os.path.split(func_frame.f_globals['__file__'])[0]
-	
-	def __enter__(self):
-		os.chdir(self.func_path)
-		return self.func(*self.args, **self.kargs)
-	
-	def __exit__(self, exc_type, exc_value, exc_trackback):
-		os.chdir(self.orig_path)
+			raise TypeError(f"the 'target' must be function or str, not '{target_type}'")
+		
+		self.orig_path = getcwd()
+		self.func_path = split(currentframe().f_back.f_globals['__file__'])[0]
 	
 	def __call__(self, *args, **kargs):
-		with run_here(self.func, *args, **kargs) as result:
-			return result
+		chdir(self.func_path)
+		
+		if hasattr(self, 'dire_offset'):
+			def result(*arg, **karg):
+				chdir(self.dire_offset)
+				func_result = args[0](*arg, **karg)
+				return func_result
+		else:
+			result = self.func(*args, **kargs)
+		
+		chdir(self.orig_path)
+		return result
